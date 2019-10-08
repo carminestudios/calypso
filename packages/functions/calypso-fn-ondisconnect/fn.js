dc75ebb1c32f17904b8ee58faaf464a2609ea66e
Patrik Nyblad <patrik.nyblad@gmail.com>
2019-10-08 22:52:20 +0900

    Broadcast a list of all remaining peers to remaining peers on disconnect

--- packages/functions/calypso-fn-ondisconnect/fn.js
+++ packages/functions/calypso-fn-ondisconnect/fn.js
@@ -1,9 +1,15 @@
-const { removeConnection } = require('@carminestudios/calypso-persistence');
+const { removeConnection, getConnections } = require('@carminestudios/calypso-persistence');
+const { broadcast } = require('@carminestudios/calypso-comms');
 
-const onDisconnect = async (connectionId) => {
+const onDisconnect = async (wsClient, connectionId) => {
   console.log('removing connection', connectionId);
   await removeConnection(connectionId);
   console.log('connection removed');
+  const connections = await getConnections();
+  const connectionIds = connections.map(connection => ({id: connection.id}));
+  await broadcast(wsClient, {
+    message: { method: 'peers', params: connectionIds },
+  });
   return Promise.resolve();
 };
 
