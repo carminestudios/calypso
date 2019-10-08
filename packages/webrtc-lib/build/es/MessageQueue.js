d03a223ba7dbd306f900d4d3941225b0244ba7a3
Fredrik Westmark <fredrik.westmark@gmail.com>
2019-10-08 13:22:48 +0900

    chore: un-add build files

--- packages/webrtc-lib/build/es/MessageQueue.js
+++
@@ -1,4 +0,0 @@
-import _classCallCheck from "@babel/runtime/helpers/classCallCheck";
-export var MessageQueue = function MessageQueue() {
-  _classCallCheck(this, MessageQueue);
-};
--- packages/webrtc-lib/build/es/index.js
+++
@@ -1,3 +0,0 @@
-export * from './BobRTC';
-export * from './BobPeerConnection';
-export * from './SignalingHandler';
